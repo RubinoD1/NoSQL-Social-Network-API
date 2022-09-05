e0aedc49b9b4c9a226e57b14d4dd93b167b3e7ac
RubinoD1 <Rubinod1@gmail.com>
2022-09-05 14:14:49 -0400

    updated server.js file and add user-controller.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(require('./routes'));
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-// Logs the mongo queries being executed
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`🌍 Connected on localhost:${PORT}`);
+});
