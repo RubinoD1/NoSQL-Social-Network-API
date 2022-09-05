47d6f57b1040ea52841f387cea44b9447116d250
RubinoD1 <Rubinod1@gmail.com>
2022-09-04 23:09:55 -0400

    started routes -- thought-routes.js file added

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
   useUnifiedTopology: true
 });
 
-// Logs mongo queries being executed
+// Logs the mongo queries being executed
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
